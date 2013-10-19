f4b19d58a6a353a84ff84c12ff8c542e1902c57c
1c0d3 <hackjack@iapphacks.com>
2013-10-19 20:21:52 +0200

    Shortened Code and updated writing

--- aslrwrite.h
+++ aslrwrite.h
@@ -21,67 +21,6 @@
 #import <mach/mach_traps.h>
 
 /*
-This Function checks if the Application has ASLR enabled.
-It gets the mach_header of the Image at Index 0.
-It then checks for the MH_PIE flag. If it is there, it returns TRUE.
-Parameters: nil
-Return: Wether it has ASLR or not
-*/
-
-__attribute__((visibility("hidden"))) bool hasASLR() {
-    
-    const struct mach_header *mach;
-    
-    mach = _dyld_get_image_header(0);
-    
-    if (mach->flags & MH_PIE) {
-	
-	//has aslr enabled
-	return TRUE;
-	
-    } else {
-	
-	//has aslr disabled
-	return FALSE;
-    }
-    
-    
-}
-
-/*
-This Function gets the vmaddr slide of the Image at Index 0.
-Parameters: nil
-Return: the vmaddr slide
-*/
-
-__attribute__((always_inline))	__attribute__((visibility("hidden"))) vm_address_t vm_get_slide()
-{
-    return _dyld_get_image_vmaddr_slide(0);
-}
-
-/*
-This Function calculates the Address if ASLR is enabled or returns the normal offset.
-Parameters: The Original Offset
-Return: Either the Offset or the New calculated Offset if ASLR is enabled
-*/
-
-__attribute__((visibility("hidden"))) vm_address_t calculateAddress(vm_address_t offset) {
-	
-    if (hasASLR()) {
-	
-	vm_address_t addr = vm_get_slide();
-	
-	return (addr + offset);
-	
-    } else {
-	
-	return offset;
-	
-    }
-    
-}
-
-/*
 This is the main Function. This is where the writing takes place.
 It declares the port as mach_task_self, calculates the offset.
 Then it changes the Protections at the offset to be able to write to it.
@@ -91,17 +30,17 @@ Parameters: the address, the data to be written, the size 2 or 4
 Return: True = Success or False = Failed
 */
 
-__attribute__((visibility("hidden"))) bool writeData(vm_address_t offset,  unsigned int data, int size) {
+__attribute__((visibility("hidden"))) bool writeData(vm_address_t offset,  unsigned long long data, int size) {
 
     //declaring variables
 
     kern_return_t err;
     mach_port_t port = mach_task_self();
-    vm_address_t address = calculateAddress(offset);
+    vm_address_t address = (offset + _dyld_get_image_vmaddr_slide(0));
 
     //set memory protections to allow us writing code there
 
-    err = vm_protect(port, (vm_address_t) address, sizeof(data), NO,VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    err = vm_protect(port, (vm_address_t) address, size, NO,VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
 
     //check if the protection fails
 
@@ -112,21 +51,25 @@ __attribute__((visibility("hidden"))) bool writeData(vm_address_t offset,  unsig
     }
     
     //write code to memory
-
-    if (size == 4) {
-  
-    *(uint32_t*)address = CFSwapInt32(data);
-
-    } else if (size == 2) {
-
-    *(uint16_t*)address = CFSwapInt16(data);
-
+    
+    err = vm_write(port, (vm_address_t)address, (vm_address_t)&data, size);
+    
+    if (err != KERN_SUCCESS) {
+        
+        return FALSE;
+        
     }
     
     //set the protections back to normal so the app can access this address as usual
 
-    err = vm_protect(port, (vm_address_t)address, sizeof(data), NO,VM_PROT_READ | VM_PROT_EXECUTE);
+    err = vm_protect(port, (vm_address_t)address, size, NO,VM_PROT_READ | VM_PROT_EXECUTE);
+    
+    if (err != KERN_SUCCESS) {
+        
+        return FALSE;
+        
+    }
 
-      return TRUE;
+    return TRUE;
 	
 }
